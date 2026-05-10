abb9d5c7e204286e07a7e92d6b51181a54dd7549
Aryaman51 <aryamanjarial@gmail.com>
2026-05-10 22:58:32 +0530

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/ubuntu/app
+
+docker stop tripspend-backend || true
+docker rm tripspend-backend || true
+
+docker build -t tripspend-backend .
+
+docker run -d \
+  --name tripspend-backend \
+  -p 5000:5000 \
+  tripspend-backend
